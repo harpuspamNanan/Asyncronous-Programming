9365b58d9ae10ca5c957c6e3004dbcd8e21283e3
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-30 02:37:00 -0400

    Concurrent Dictionaries

--- AsyncronousProgramming/Program.cs
+++ AsyncronousProgramming/Program.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncronousProgramming
 {
     class Program
     {
+        static ConcurrentDictionary<int, int> items = new ConcurrentDictionary<int, int>();
+        // Cant use a normal Dictionary because then the key for 2 sets would be same, which is not possible
+
         static void Main(string[] args)
         {
-            Hashtable table = new Hashtable();
-            table.Add("title", "Website");
-            var s = (String)table["title"];         // Accessing it through Unboxing
-            Console.WriteLine(s); 
+            Thread thread1 = new Thread(new ThreadStart(AddItem));
+            Thread thread2 = new Thread(new ThreadStart(AddItem));
+
+            thread1.Start();
+            thread2.Start();
             Console.ReadLine();
+        }
+
+        static void AddItem()
+        {
+            items.TryAdd(1, 2);
+            Console.WriteLine(items.Count);
         }
     }
 }
